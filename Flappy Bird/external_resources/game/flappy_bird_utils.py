a4757684606b906dc96271706cdc0c130552b855
Xingyang Ni <nixingyang@users.noreply.github.com>
2018-10-25 19:26:33 +0300

    Fix issues of the file paths.

--- Flappy Bird/external_resources/game/flappy_bird_utils.py
+++ Flappy Bird/external_resources/game/flappy_bird_utils.py
@@ -1,37 +1,41 @@
-import pygame
+import os
 import sys
+import pygame
+
+ASSETS_FOLDER_PATH = os.path.abspath(os.path.join(__file__, '../../assets'))
+
 def load():
     # path of player with different states
     PLAYER_PATH = (
-            'assets/sprites/redbird-upflap.png',
-            'assets/sprites/redbird-midflap.png',
-            'assets/sprites/redbird-downflap.png'
+            os.path.join(ASSETS_FOLDER_PATH, 'sprites/redbird-upflap.png'),
+            os.path.join(ASSETS_FOLDER_PATH, 'sprites/redbird-midflap.png'),
+            os.path.join(ASSETS_FOLDER_PATH, 'sprites/redbird-downflap.png')
     )
 
     # path of background
-    BACKGROUND_PATH = 'assets/sprites/background-black.png'
+    BACKGROUND_PATH = os.path.join(ASSETS_FOLDER_PATH, 'sprites/background-black.png')
 
     # path of pipe
-    PIPE_PATH = 'assets/sprites/pipe-green.png'
+    PIPE_PATH = os.path.join(ASSETS_FOLDER_PATH, 'sprites/pipe-green.png')
 
     IMAGES, SOUNDS, HITMASKS = {}, {}, {}
 
     # numbers sprites for score display
     IMAGES['numbers'] = (
-        pygame.image.load('assets/sprites/0.png').convert_alpha(),
-        pygame.image.load('assets/sprites/1.png').convert_alpha(),
-        pygame.image.load('assets/sprites/2.png').convert_alpha(),
-        pygame.image.load('assets/sprites/3.png').convert_alpha(),
-        pygame.image.load('assets/sprites/4.png').convert_alpha(),
-        pygame.image.load('assets/sprites/5.png').convert_alpha(),
-        pygame.image.load('assets/sprites/6.png').convert_alpha(),
-        pygame.image.load('assets/sprites/7.png').convert_alpha(),
-        pygame.image.load('assets/sprites/8.png').convert_alpha(),
-        pygame.image.load('assets/sprites/9.png').convert_alpha()
+        pygame.image.load(os.path.join(ASSETS_FOLDER_PATH, 'sprites/0.png')).convert_alpha(),
+        pygame.image.load(os.path.join(ASSETS_FOLDER_PATH, 'sprites/1.png')).convert_alpha(),
+        pygame.image.load(os.path.join(ASSETS_FOLDER_PATH, 'sprites/2.png')).convert_alpha(),
+        pygame.image.load(os.path.join(ASSETS_FOLDER_PATH, 'sprites/3.png')).convert_alpha(),
+        pygame.image.load(os.path.join(ASSETS_FOLDER_PATH, 'sprites/4.png')).convert_alpha(),
+        pygame.image.load(os.path.join(ASSETS_FOLDER_PATH, 'sprites/5.png')).convert_alpha(),
+        pygame.image.load(os.path.join(ASSETS_FOLDER_PATH, 'sprites/6.png')).convert_alpha(),
+        pygame.image.load(os.path.join(ASSETS_FOLDER_PATH, 'sprites/7.png')).convert_alpha(),
+        pygame.image.load(os.path.join(ASSETS_FOLDER_PATH, 'sprites/8.png')).convert_alpha(),
+        pygame.image.load(os.path.join(ASSETS_FOLDER_PATH, 'sprites/9.png')).convert_alpha()
     )
 
     # base (ground) sprite
-    IMAGES['base'] = pygame.image.load('assets/sprites/base.png').convert_alpha()
+    IMAGES['base'] = pygame.image.load(os.path.join(ASSETS_FOLDER_PATH, 'sprites/base.png')).convert_alpha()
 
     # sounds
     if 'win' in sys.platform:
@@ -39,11 +43,11 @@ def load():
     else:
         soundExt = '.ogg'
 
-    SOUNDS['die']    = pygame.mixer.Sound('assets/audio/die' + soundExt)
-    SOUNDS['hit']    = pygame.mixer.Sound('assets/audio/hit' + soundExt)
-    SOUNDS['point']  = pygame.mixer.Sound('assets/audio/point' + soundExt)
-    SOUNDS['swoosh'] = pygame.mixer.Sound('assets/audio/swoosh' + soundExt)
-    SOUNDS['wing']   = pygame.mixer.Sound('assets/audio/wing' + soundExt)
+    SOUNDS['die'] = pygame.mixer.Sound(os.path.join(ASSETS_FOLDER_PATH, 'audio/die') + soundExt)
+    SOUNDS['hit'] = pygame.mixer.Sound(os.path.join(ASSETS_FOLDER_PATH, 'audio/hit') + soundExt)
+    SOUNDS['point'] = pygame.mixer.Sound(os.path.join(ASSETS_FOLDER_PATH, 'audio/point') + soundExt)
+    SOUNDS['swoosh'] = pygame.mixer.Sound(os.path.join(ASSETS_FOLDER_PATH, 'audio/swoosh') + soundExt)
+    SOUNDS['wing'] = pygame.mixer.Sound(os.path.join(ASSETS_FOLDER_PATH, 'audio/wing') + soundExt)
 
     # select random background sprites
     IMAGES['background'] = pygame.image.load(BACKGROUND_PATH).convert()
@@ -83,5 +87,5 @@ def getHitmask(image):
     for x in range(image.get_width()):
         mask.append([])
         for y in range(image.get_height()):
-            mask[x].append(bool(image.get_at((x,y))[3]))
+            mask[x].append(bool(image.get_at((x, y))[3]))
     return mask
