d7a8cdb7eb8a384c22043b82ab3e62f1d3b52707
Xingyang Ni <nixingyang@users.noreply.github.com>
2018-10-29 21:32:00 +0200

    Process the images.

--- Flappy Bird/solution.py
+++ Flappy Bird/solution.py
@@ -6,15 +6,16 @@ session = tf.Session(config=config)
 from keras import backend as K
 K.set_session(session)
 
-import os
-import sys
-sys.path.append(os.path.abspath(os.path.join(__file__, "../external_resources/game")))
-from wrapped_flappy_bird import GameState  # @UnresolvedImport pylint: disable=import-error
-
+import cv2
 import numpy as np
 from keras.layers import Input, Conv2D, Activation, BatchNormalization, MaxPooling2D, GlobalAveragePooling2D, Dropout, Dense
 from keras.models import Model
 from keras.utils import plot_model
+
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(__file__, "../external_resources/game")))
+from wrapped_flappy_bird import GameState  # @UnresolvedImport pylint: disable=import-error
 
 # Properties of frames
 FRAME_HEIGHT, FRAME_WIDTH = 100, 60
@@ -45,6 +46,18 @@ def init_model():
 def run():
     print("Initiating the model ...")
     model = init_model()
+
+    print("Initiating the GameState ...")
+    game_state_object = GameState()
+
+    # Take "do nothing" action
+    vanilla_image_content, _, is_crashed = game_state_object.frame_step(input_actions=[1, 0])
+    processed_image_content = np.swapaxes(vanilla_image_content, 0, 1)
+    processed_image_content = cv2.cvtColor(processed_image_content, cv2.COLOR_RGB2GRAY)
+    processed_image_content = cv2.resize(processed_image_content, (FRAME_WIDTH, FRAME_HEIGHT))
+    processed_image_content = processed_image_content.astype(np.float32) / 255
+    accumulated_image_content = np.stack([processed_image_content] * ACCUMULATED_FRAME_NUM, axis=-1)
+    accumulated_image_content = np.expand_dims(accumulated_image_content, axis=0)
 
     print("All done!")
 
