f13f41b9fdd0160ebffd9b74d9a28bcab7a9f73e
nixingyang <nixingyang@users.noreply.github.com>
2020-11-28 16:40:54 +0200

    Get the folder path of all repositories.

--- Summarize Commits/solution.py
+++ Summarize Commits/solution.py
@@ -9,9 +9,17 @@ flags.DEFINE_string("author", "nixingyang", "Author.")
 flags.DEFINE_string("start_date", "2020-03-01", "Start date.")
 flags.DEFINE_string("end_date", "2020-06-30", "End date.")
 flags.DEFINE_string("hours", "7,35", "Working hours.")
+FLAGS = flags.FLAGS
 
 
 def main(_):
+    # Get the folder path of all repositories
+    repository_folder_path_list = []
+    for item in sorted(os.listdir(FLAGS.root_folder_path)):
+        repository_folder_path = os.path.join(FLAGS.root_folder_path, item)
+        if os.path.isdir(repository_folder_path):
+            repository_folder_path_list.append(repository_folder_path)
+
     print("All done!")
 
 
