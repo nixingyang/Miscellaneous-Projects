5ba5b7f78d65391e103ee77c509272580dbda6a7
nixingyang <nixingyang@users.noreply.github.com>
2021-01-06 15:26:47 +0200

    Update the pdf file if needed.

--- Update arXiv/solution.py
+++ Update arXiv/solution.py
@@ -2,6 +2,7 @@ import glob
 import os
 
 import arxiv
+import requests
 from absl import app, flags
 
 flags.DEFINE_string("root_folder_path", "~/Downloads/Papers",
@@ -15,6 +16,7 @@ def main(_):
         os.path.expanduser(FLAGS.root_folder_path))
     source_file_path_list = sorted(
         glob.glob(os.path.join(root_folder_path, "**/*.pdf"), recursive=True))
+    get_relative_path = lambda path: path[len(root_folder_path) + 1:]
 
     # Iterate over the pdf files
     for source_file_path in source_file_path_list:
@@ -24,7 +26,24 @@ def main(_):
         id_without_version = id_without_version.split("v")[0]
         query_result_list = arxiv.query(id_list=[id_without_version])
         if not query_result_list:
+            print("Failed to analyze {}.".format(
+                get_relative_path(source_file_path)))
             continue
+
+        # Update the pdf file if needed
+        query_result = query_result_list[0]
+        title = query_result["title"]
+        pdf_url = query_result["pdf_url"]
+        id_with_version = pdf_url.split("/")[-1]
+        target_file_path = os.path.abspath(
+            os.path.join(source_file_path, "..",
+                         f"{id_with_version} {title}.pdf"))
+        if not os.path.isfile(target_file_path):
+            print("Updating {} with {}".format(
+                get_relative_path(source_file_path),
+                get_relative_path(target_file_path)))
+            open(target_file_path, "wb").write(requests.get(pdf_url).content)
+            os.remove(source_file_path)
 
     print("All done!")
 
