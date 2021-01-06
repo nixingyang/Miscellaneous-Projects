3ac4682ddba0eef32c432cd7a6e2d106880a23ed
nixingyang <nixingyang@users.noreply.github.com>
2021-01-06 15:15:38 +0200

    Minor fixes.

--- Update arXiv/solution.py
+++ Update arXiv/solution.py
@@ -13,16 +13,17 @@ def main(_):
     # Find the pdf files within root_folder_path
     root_folder_path = os.path.abspath(
         os.path.expanduser(FLAGS.root_folder_path))
-    file_path_list = sorted(
+    source_file_path_list = sorted(
         glob.glob(os.path.join(root_folder_path, "**/*.pdf"), recursive=True))
 
     # Iterate over the pdf files
-    for file_path in file_path_list:
+    for source_file_path in source_file_path_list:
         # Query with id_without_version
-        id_without_version = ".".join(
-            os.path.basename(file_path).split(".")[:2]).split("v")[0]
-        query_results = arxiv.query(id_list=[id_without_version])
-        if not query_results:
+        id_without_version = os.path.basename(source_file_path).split(" ")[0]
+        id_without_version = ".".join(id_without_version.split(".")[:2])
+        id_without_version = id_without_version.split("v")[0]
+        query_result_list = arxiv.query(id_list=[id_without_version])
+        if not query_result_list:
             continue
 
     print("All done!")
